4a5e1cdf74702e95ce2bd2d65c493c8afb10c31d
YKulinichev <dp100191kjm@gmail.com>
2021-03-03 13:00:18 +0100

    Changes to the database connection information

--- MyGame/src/playerBase/Configs.java
+++ MyGame/src/playerBase/Configs.java
@@ -3,8 +3,8 @@ package playerBase;
 public class Configs {
 protected String dbHost = "127.0.0.1";
 protected String dbPort = "3306";
-protected String dbUser = "root";
-protected String dbPass = "root";
+protected String dbUser = "hbstudent";
+protected String dbPass = "hbstudent";
 protected String dbName = "playerbase";
 
 
